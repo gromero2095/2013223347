071aabc94b381f0f94d09582b9400cb3deeace08
Clase <Clase>
2017-04-05 19:10:27 -0500

    Avance 1

--- 2013223347/2013223347-GROMERO/cinturon.cs
+++ 2013223347/2013223347-GROMERO/cinturon.cs
@@ -8,5 +8,21 @@ namespace _2013223347_GROMERO
 {
     class cinturon
     {
+        
+
+        public int Metraje { get; set; }
+        public string NumSerie { get; set; }
+
+
+        public cinturon()
+        {
+        }
+
+        public cinturon(string _NumSerie, int _Metraje)
+        {
+            Metraje = _Metraje;
+            NumSerie = _NumSerie;
+        }
+
     }
 }
